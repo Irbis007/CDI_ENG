54ae90999514c9097c5be222b4e7f28309b3cc27
Irbis007 <suskodomk@gmail.com>
2023-09-15 14:28:00 +0300

    34235

--- js/script.js
+++ js/script.js
@@ -1,95 +1,115 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const questionWrapper = document.querySelectorAll(".question-wrapper");
 
-questionWrapper.forEach((item, i) => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("active");
+    questionWrapper.forEach((item, i) => {
+        item.addEventListener("click", () => {
+            item.classList.toggle("active");
+        });
     });
-});
 
-const expertTabButtons = document.querySelectorAll(".expert .tab-button");
-const expertTabContents = document.querySelectorAll(".expert .tab-content");
+    const expertTabButtons = document.querySelectorAll(".expert .tab-button");
+    const expertTabContents = document.querySelectorAll(".expert .tab-content");
 
-expertTabButtons.forEach((item, i) => {
-    item.addEventListener("click", () => {
-        removeActiveExpert();
-        item.classList.add("active");
-        expertTabContents[i].classList.add("active");
+    expertTabButtons.forEach((item, i) => {
+        item.addEventListener("click", () => {
+            removeActiveExpert();
+            item.classList.add("active");
+            expertTabContents[i].classList.add("active");
+        });
     });
-});
 
-function removeActiveExpert() {
-    for (let i = 0; i < expertTabButtons.length; i++) {
-        expertTabButtons[i].classList.remove("active");
-        expertTabContents[i].classList.remove("active");
-    }
-}
-
-const feedbackTabButtons = document.querySelectorAll(".feedback .tab-button");
-const feedbackTabContentsImg = document.querySelectorAll(".feedback .tab-content_img");
-const feedbackTabContentsText = document.querySelectorAll(".feedback .tab-content_text");
-
-feedbackTabButtons.forEach((item, i) => {
-    item.addEventListener("click", () => {
-        removeActiveFeedback();
-        item.classList.add("active");
-        feedbackTabContentsImg[i].classList.add("active");
-        feedbackTabContentsText[i].classList.add("active");
-    });
-});
-
-function removeActiveFeedback() {
-    for (let i = 0; i < feedbackTabButtons.length; i++) {
-        feedbackTabButtons[i].classList.remove("active");
-        feedbackTabContentsImg[i].classList.remove("active");
-        feedbackTabContentsText[i].classList.remove("active");
-    }
-}
-
-const hamburger = document.querySelector(".hamburger");
-const headerNav = document.querySelector(".header-nav");
-
-hamburger.addEventListener("click", () => {
-    headerNav.classList.toggle("active");
-});
-
-const time = 5000;
-const step = 1;
-
-function outNum(num, elem) {
-    let e = elem;
-    n = 0;
-    let t = Math.round(time / (num / step));
-    let interval = setInterval(() => {
-        n = n + step;
-        if (n == num || n > num) {
-            clearInterval(interval);
+    function removeActiveExpert() {
+        for (let i = 0; i < expertTabButtons.length; i++) {
+            expertTabButtons[i].classList.remove("active");
+            expertTabContents[i].classList.remove("active");
         }
-        e.innerHTML = `${n}+`;
-    }, t);
-}
+    }
 
-const advanceNumbers = document.querySelectorAll(".advance-number");
-const advancesNumber = [20, 8, 10];
+    const feedbackTabButtons = document.querySelectorAll(".feedback .tab-button");
+    const feedbackTabContentsImg = document.querySelectorAll(".feedback .tab-content_img");
+    const feedbackTabContentsText = document.querySelectorAll(".feedback .tab-content_text");
 
-
-
-function handleIntersection(entries, observer) {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            advanceNumbers.forEach((item, i) => {
-                outNum(advancesNumber[i], item);
-            });            
-            observer.unobserve(advances);
-        }
+    feedbackTabButtons.forEach((item, i) => {
+        item.addEventListener("click", () => {
+            removeActiveFeedback();
+            item.classList.add("active");
+            feedbackTabContentsImg[i].classList.add("active");
+            feedbackTabContentsText[i].classList.add("active");
+        });
     });
-}
 
-const advances = document.querySelector(".advances");
-const observer = new IntersectionObserver(handleIntersection);
-observer.observe(advances);
+    function removeActiveFeedback() {
+        for (let i = 0; i < feedbackTabButtons.length; i++) {
+            feedbackTabButtons[i].classList.remove("active");
+            feedbackTabContentsImg[i].classList.remove("active");
+            feedbackTabContentsText[i].classList.remove("active");
+        }
+    }
+
+    const hamburger = document.querySelector(".hamburger");
+    const headerNav = document.querySelector(".header-nav");
+
+    hamburger.addEventListener("click", () => {
+        headerNav.classList.toggle("active");
+    });
+
+    const time = 5000;
+    const timeStep = 1;
+
+    function outNum(num, elem) {
+        let e = elem;
+        n = 0;
+        let t = Math.round(time / (num / timeStep));
+        let interval = setInterval(() => {
+            n = n + timeStep;
+            if (n == num || n > num) {
+                clearInterval(interval);
+            }
+            e.innerHTML = `${n}+`;
+        }, t);
+    }
+
+    const advanceNumbers = document.querySelectorAll(".advance-number");
+    const advancesNumber = [20, 8, 10];
+
+    function handleIntersection(entries, observer) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                advanceNumbers.forEach((item, i) => {
+                    outNum(advancesNumber[i], item);
+                });
+                observer.unobserve(advances);
+            }
+        });
+    }
+
+    const advances = document.querySelector(".advances");
+    const observer = new IntersectionObserver(handleIntersection);
+    observer.observe(advances);
+
+    const sliderWrapper = document.querySelector(".course-window");
+    const sliderItem = document.querySelectorAll(".course-wrapper");
+
+    const arrowPrev = document.querySelector(".course-arrow.prev");
+    const arrowNext = document.querySelector(".course-arrow.next");
+    
+    let sliderStep = 0;
+
+    arrowPrev.addEventListener('click', ()=>{
+        if(sliderStep > 0){
+            sliderStep--
+        }
+        sliderWrapper.style.transform = `translateX(${-sliderStep*300}px)`
+        console.log(sliderStep)
+    })
+    
+    arrowNext.addEventListener('click', ()=>{
+        if(sliderStep <= sliderItem.length -5){
+            sliderStep++
+        }
+        sliderWrapper.style.transform = `translateX(${-sliderStep*300}px)`
+        console.log(sliderStep)
+    })
 
 
-
-
-})
+});
