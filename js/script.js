becb2325530bee6aea09eb4371b3a84cce8d3666
Irbis007 <suskodomk@gmail.com>
2023-09-17 04:58:55 +0300

    add slider for expert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     // QUESTION ACCARDEON
     const questionWrapper = document.querySelectorAll(".question-wrapper");
 
@@ -9,26 +8,32 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-
     // EXPERT TAB
-    const expertTabButtons = document.querySelectorAll(".expert .tab-button");
-    const expertTabContents = document.querySelectorAll(".expert .tab-content");
+    const firstExpertTabButtons = document.querySelectorAll(".first-expert .tab-button");
+    const firstExpertTabContents = document.querySelectorAll(".first-expert .tab-content");
 
-    expertTabButtons.forEach((item, i) => {
-        item.addEventListener("click", () => {
-            removeActiveExpert();
-            item.classList.add("active");
-            expertTabContents[i].classList.add("active");
+    const secondExpertTabButtons = document.querySelectorAll(".second-expert .tab-button");
+    const secondExpertTabContents = document.querySelectorAll(".second-expert .tab-content");
+
+    function addActiveExpert(tabButtons, tabContent) {
+        tabButtons.forEach((item, i) => {
+            item.addEventListener("click", () => {
+                removeActiveExpert(tabButtons, tabContent);
+                item.classList.add("active");
+                tabContent[i].classList.add("active");
+            });
         });
-    });
+    }
 
-    function removeActiveExpert() {
-        for (let i = 0; i < expertTabButtons.length; i++) {
-            expertTabButtons[i].classList.remove("active");
-            expertTabContents[i].classList.remove("active");
+    function removeActiveExpert(tabButtons, tabContent) {
+        for (let i = 0; i < tabButtons.length; i++) {
+            tabButtons[i].classList.remove("active");
+            tabContent[i].classList.remove("active");
         }
     }
 
+    addActiveExpert(firstExpertTabButtons, firstExpertTabContents);
+    addActiveExpert(secondExpertTabButtons, secondExpertTabContents);
 
     // FEEDBACK TAB
     const feedbackTabButtons = document.querySelectorAll(".feedback .tab-button");
@@ -52,8 +57,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-
-    // NUMBER ANIMATION
     const hamburger = document.querySelector(".hamburger");
     const headerNav = document.querySelector(".header-nav");
 
@@ -61,48 +64,59 @@ document.addEventListener("DOMContentLoaded", () => {
         headerNav.classList.toggle("active");
     });
 
-    const time = 5000;
-    const timeStep = 1;
+    // NUMBER ANIMATION
+    const time = 10000;
+    const step = 1;
 
     function outNum(num, elem) {
-        let e = elem;
         n = 0;
-        let t = Math.round(time / (num / timeStep));
+        let t = Math.round(time / num);
         let interval = setInterval(() => {
-            n = n + timeStep;
-            if (n == num || n > num) {
+            n = n + step;
+            if (n == num) {
                 clearInterval(interval);
+            } else if (n > num) {
+                elem.innerHTML = `${num}+`;
+                clearInterval(interval);
+                return;
             }
-            e.innerHTML = `${n}+`;
+            elem.innerHTML = `${n}+`;
         }, t);
     }
 
-    const advanceNumbers = document.querySelectorAll(".advance-number");
-    const advancesNumber = [20, 8, 10];
+    function startObserve(arrOfNumber, listOfNumberWrapper, numberParrent) {
+        function handleIntersection(entries, observer) {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    listOfNumberWrapper.forEach((item, i) => {
+                        outNum(arrOfNumber[i], item);
+                    });
+                    observer.unobserve(numberParrent);
+                }
+            });
+        }
 
-    function handleIntersection(entries, observer) {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                advanceNumbers.forEach((item, i) => {
-                    outNum(advancesNumber[i], item);
-                });
-                observer.unobserve(advances);
-            }
-        });
+        const observer = new IntersectionObserver(handleIntersection);
+        observer.observe(numberParrent);
     }
 
-    const advances = document.querySelector(".advances");
-    const observer = new IntersectionObserver(handleIntersection);
-    observer.observe(advances);
-
-
+    startObserve(
+        [20, 9, 11],
+        document.querySelectorAll(".first-expert .advance-number"),
+        document.querySelector(".first-expert .advances")
+    );
+    startObserve(
+        [20, 1, 30],
+        document.querySelectorAll(".second-expert .advance-number"),
+        document.querySelector(".second-expert .advances")
+    );
 
     // SLIDER
-    const sliderWrapper = document.querySelector(".course-window");
-    const sliderItem = document.querySelectorAll(".course-wrapper");
+    const sliderWrapper = document.querySelector(".course .slider-window");
+    const sliderItem = document.querySelectorAll(".course .course-wrapper");
 
-    const arrowPrev = document.querySelector(".course-arrow.prev");
-    const arrowNext = document.querySelector(".course-arrow.next");
+    const arrowPrev = document.querySelector(".course .slider-arrow.prev");
+    const arrowNext = document.querySelector(".course .slider-arrow.next");
 
     let stepWidth;
 
@@ -118,14 +132,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let sliderStep = 0;
 
-    console.log(stepWidth);
-
     arrowPrev.addEventListener("click", () => {
         if (sliderStep > 0) {
             sliderStep--;
         }
+        console.log(1);
         sliderWrapper.style.transform = `translateX(${-sliderStep * stepWidth}px)`;
     });
+    
 
     arrowNext.addEventListener("click", () => {
         if (window.innerWidth > 991) {
@@ -147,4 +161,56 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         sliderWrapper.style.transform = `translateX(${-sliderStep * stepWidth}px)`;
     });
+
+
+    const experSliderWrapper = document.querySelector(".expert .slider-window");
+    const experSliderItem = document.querySelectorAll(".expert .expert-slider_content");
+
+    const experArrowPrev = document.querySelector(".expert .slider-arrow.prev");
+    const expertArrowNext = document.querySelector(".expert .slider-arrow.next");
+
+
+    let expertStepWidth;
+
+    if (window.innerWidth > 991) {
+        expertStepWidth = experSliderItem[0].clientWidth + 40;
+    } else if (window.innerWidth > 767 && window.innerWidth < 991) {
+        expertStepWidth = experSliderItem[0].clientWidth + 30;
+    } else if (window.innerWidth > 576 && window.innerWidth < 767) {
+        expertStepWidth = experSliderItem[0].clientWidth + 20;
+    } else if (window.innerWidth < 576) {
+        expertStepWidth = experSliderItem[0].clientWidth + 18 + 33;
+    }
+
+    let experSliderStep = 0;
+
+    experArrowPrev.addEventListener("click", () => {
+        if (experSliderStep > 0) {
+            experSliderStep--;
+        }
+        experSliderWrapper.style.transform = `translateX(${-experSliderStep * expertStepWidth}px)`;
+    });
+    
+
+    expertArrowNext.addEventListener("click", () => {
+        if (window.innerWidth > 991) {
+            if (experSliderStep <= experSliderItem.length - 13) {
+                experSliderStep++;
+            }
+        } else if (window.innerWidth > 767 && window.innerWidth < 991) {
+            if (experSliderStep <= experSliderItem.length - 4) {
+                experSliderStep++;
+            }
+        } else if (window.innerWidth > 576 && window.innerWidth < 767) {
+            if (experSliderStep <= experSliderItem.length - 3) {
+                experSliderStep++;
+            }
+        } else if (window.innerWidth < 576) {
+            if (experSliderStep <= experSliderItem.length - 2) {
+                experSliderStep++;
+            }
+        }
+        experSliderWrapper.style.transform = `translateX(${-experSliderStep * expertStepWidth}px)`;
+    });
+
 });
